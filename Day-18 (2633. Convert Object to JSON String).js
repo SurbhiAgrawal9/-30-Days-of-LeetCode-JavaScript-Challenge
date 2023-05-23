3105ffd95d47143fa117675c859e71d9e811a296
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2023-05-24 03:08:31 +0530

    Create Day-18 (2633. Convert Object to JSON String).js

---
+++ Day-18 (2633. Convert Object to JSON String).js
@@ -0,0 +1,20 @@
+/**
+ * @param {any} object
+ * @return {string}
+ */
+var jsonStringify = function(object) {
+  if (typeof object === 'string') {
+    return '"' + object + '"';
+  } else if (typeof object === 'number' || typeof object === 'boolean' || object === null) {
+    return String(object);
+  } else if (Array.isArray(object)) {
+    var arrayElements = object.map(jsonStringify).join(',');
+    return '[' + arrayElements + ']';
+  } else if (typeof object === 'object') {
+    var objectProperties = Object.keys(object).map(function(key) {
+      var propertyValue = jsonStringify(object[key]);
+      return '"' + key + '":' + propertyValue;
+    }).join(',');
+    return '{' + objectProperties + '}';
+  }
+};
